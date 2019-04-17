e8ae66fe539e96bea1cbc75e4da94aa98f01ccd8
Pan32 <38963211+francoZuniga@users.noreply.github.com>
2019-04-17 11:54:10 -0300

    Create perro.java

---
+++ tda/src/tp7/perro.java
@@ -0,0 +1,41 @@
+package tp7;
+public class perro {
+   public String nombre, apellidoDueño, raza;
+   public int año, costo;
+   public boolean cuidado;
+   //constructores
+   public perro(String argNombre,String argApellidoDueño,String argRaza){
+       this.nombre = argNombre;
+       this.apellidoDueño = argApellidoDueño;
+       this.raza = argRaza;
+   }
+   
+   public perro(String argNombre, String argApellidoDuño, String argRaza, int argAño,int argCosto, boolean argCuidado){
+       this.nombre = argNombre;
+       this.apellidoDueño = argApellidoDuño;
+       this.raza = argRaza;
+       this.costo = argCosto;
+       this.año = argAño;
+       this.cuidado = argCuidado;
+   }
+   //observadores
+   public String getNombre(){
+       return this.nombre;
+   }
+   
+   public String getApellidoDueño(){
+       return this.apellidoDueño;
+   }
+   
+   public String getRaza(){
+       return this.raza;
+   }
+   
+   public int getPrecioDiario(){
+       return this.costo;
+   }
+   
+   public int getAño(){
+       return this.año;
+   }
+}
